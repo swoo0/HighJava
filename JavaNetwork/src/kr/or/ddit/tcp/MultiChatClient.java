a0a0cbdbfe0be2310c5f86112442977163d675a1
Woo <swoo0naver.com>
2021-12-20 18:43:19 +0900

    2021-12-20

---
+++ JavaNetwork/src/kr/or/ddit/tcp/MultiChatClient.java
@@ -0,0 +1,104 @@
+package kr.or.ddit.tcp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MultiChatClient {
+	Scanner scan = new Scanner(System.in);
+	private String nickName; // 대화명
+
+	// 시작
+	public void clientStart() {
+		// 대화명 입력받기
+		System.out.print("대화명 >> ");
+		nickName = scan.next();
+
+		Socket socket = null;
+		try {
+			socket = new Socket("192.168.44.2", 7777);
+			System.out.println("서버에 연결되었습니다.");
+
+			// 송신용 스레드 생성
+			ClientSender sender = new ClientSender(socket, nickName);
+			sender.start();
+			
+			// 수신용 스레드 생성
+			ClientReceiver receiver = new ClientReceiver(socket);
+			receiver.start();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// 메시지를 전송하는 Thread 클래스
+	class ClientSender extends Thread {
+		private DataOutputStream dos;
+		private String name;
+		private Scanner scan = new Scanner(System.in);
+
+		public ClientSender(Socket socket, String name) {
+			this.name = name;
+
+			try {
+				dos = new DataOutputStream(socket.getOutputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			try {
+				// 시작하자 마자 자신의 대화명을 서버로 전송
+				if (dos != null) {
+					dos.writeUTF(name);
+				}
+
+				while (dos != null) {
+					// 키보드로 입력받은 메시지를 서버로 전송
+					dos.writeUTF(scan.nextLine());
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+
+			}
+		}
+
+	}
+
+	// 수신용 Thread클래스 정의
+	class ClientReceiver extends Thread {
+		private DataInputStream dis;
+
+		// 생성자
+		public ClientReceiver(Socket socket) {
+			try {
+				dis = new DataInputStream(socket.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			while (dis != null) {
+				try {
+					// 서버로 부터 수신한 메시지 출력하기
+					System.out.println(dis.readUTF());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new MultiChatClient().clientStart();
+	}
+}
--- JavaNetwork/src/kr/or/ddit/tcp/MultiChatServer.java
+++ JavaNetwork/src/kr/or/ddit/tcp/MultiChatServer.java
@@ -23,39 +23,40 @@ public class MultiChatServer {
 	public void serverStart() {
 		Socket socket = null;
 
-		// try문 내부에서 생성하고 finally에서 close()로 자원 반납 해주던 것을 
+		// try문 내부에서 생성하고 finally에서 close()로 자원 반납 해주던 것을
 		// try() 문법은 () 내부에서 생성한 자원 들을 자동으로 반납해준다.
 		try (ServerSocket serverSocket = new ServerSocket(7777)) {
 			System.out.println("서버가 시작되었습니다.");
-			
+
 			while (true) {
 				// 클라이언트의 접속을 대기한다.
 				socket = serverSocket.accept();
-				
+
 				System.out.println("[" + socket.getInetAddress() + " : " + socket.getPort() + "] 에서 접속하였습니다.");
-				
+
 				// 메시지 전송 처리를 하는 스레드 생성 및 실행
 				ServerReceiver receiver = new ServerReceiver(socket);
 				receiver.start();
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/**
 	 * 대화방 즉, Map에 저장된 전체 유저에게 안내메시지를 전송하는 메서드
-	 * @param msg 안내메시지
+	 * 
+	 * @param msg
+	 *            안내메시지
 	 */
 	public void sendMessage(String msg) {
 		// Map에 저장된 유저의 대화명 리스트 추출(key값 구하기)
 		Iterator<String> it = clients.keySet().iterator();
 		while (it.hasNext()) {
 			try {
-				String name = it.next();  // 대화명 구하기
-				
+				String name = it.next(); // 대화명 구하기
+
 				// 대화명에 해당하는 Socket의 OutputStream객체 가져오기
 				DataOutputStream dos = new DataOutputStream(clients.get(name).getOutputStream());
 				dos.writeUTF(msg); // 메시지 보내기
@@ -64,18 +65,18 @@ public class MultiChatServer {
 			}
 		}
 	}
-	
+
 	public void sendMessage(String msg, String from) {
 		sendMessage("[" + from + "]" + msg);
 	}
-	
+
 	// 서버에서 클라이언트 메시지를 전송할 Thread를 Inner Class로 정의함.
 	// Inner클래스에서는 부모 클래스의 멤버들을 직접 사용할 수 있다.
 	class ServerReceiver extends Thread {
 		private Socket socket;
 		private DataInputStream dis;
 		private String name;
-		
+
 		public ServerReceiver(Socket socket) {
 			this.socket = socket;
 			try {
@@ -84,70 +85,46 @@ public class MultiChatServer {
 				e.printStackTrace();
 			}
 		}
-		
+
 		@Override
 		public void run() {
 			try {
 				// 서버에서는 클라이언트가 보내는 최초의 메시지 즉, 대화명을
 				// 수신해야 한다.
 				name = dis.readUTF();
-				
+
 				// 대화명을 받아서 다른 모든 클라이언트에게 대화방 참여
 				// 메시지를 보낸다.
 				sendMessage("#" + name + "님이 입장했습니다.");
-				
+
 				// 대화명과 소켓정보를 Map에 저장한다.
 				clients.put(name, socket);
 				System.out.println("현재 서버 접속자 수는 " + clients.size() + "명 입니다.");
-				
+
 				// 이 후의 메시지 처리는 반복문으로 처리한다.
 				// 한 클라이언트가 보낸 메시지를 다른 모든 클라이언트에게 보내준다.
 				while (dis != null) {
 					sendMessage(dis.readUTF(), name);
 				}
-				
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
 				// 이 finally영역이 실행된다는 것은 클라이언트의 접속이
 				// 종료되었다는 의미이다.
 				sendMessage(name + "님이 나가셨습니다.");
-				
+
 				// Map에서 해당 대화명을 삭제한다.
 				clients.remove(name);
-				
+
 				System.out.println("[" + socket.getInetAddress() + " : " + socket.getPort() + "]에서 접속을 종료했습니다.");
 				System.out.println("현재 접속자 수는 " + clients.size() + "명 입니다.");
 			}
 		}
-		
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public static void main(String[] args) {
+		new MultiChatServer().serverStart();
+	}
 }
